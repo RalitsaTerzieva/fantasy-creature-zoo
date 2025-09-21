cf4c37e5800f2f56e0bd5a6d1f68b8a3750b9791
Ralica Popova <ralicaterzieva90@gmail.com>
2025-09-21 19:00:38 +0300

    Create a creature

--- controllers/createController.js
+++ controllers/createController.js
@@ -4,3 +4,8 @@ export const getCreatures = async (req, res) => {
   const creatures = await Creature.findAll({ include: Habitat });
   res.json(creatures);
 };
+
+export const addCreature = async (req, res) => {
+    const creature = await Creature.create(req.body);
+    res.status(201).json(creature);
+};
