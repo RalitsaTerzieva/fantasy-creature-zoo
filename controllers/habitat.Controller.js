bbeb40d5b54bff5d0acf669fd309fb380952cf60
Ralica Popova <ralicaterzieva90@gmail.com>
2025-09-23 19:02:49 +0300

    Create habitats

--- controllers/habitat.Controller.js
+++ controllers/habitat.Controller.js
@@ -4,3 +4,8 @@ export const getHabitats = async (req, res) => {
   const habitats = await Habitat.findAll();
   res.json(habitats);
 };
+
+export const addHabitat = async (req, res) => {
+  const habitat = await Habitat.create(req.body);
+  res.status(201).json(habitat);
+};
