faae8f07595ecde0abe60eb135296cf26a953583
Ralica Popova <ralicaterzieva90@gmail.com>
2025-10-05 16:52:55 +0300

    Connect to the correct database

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,6 +5,7 @@ dotenv.config();
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
   host: "localhost",
   dialect: "postgres",
+  database: "fantasy_zoo"
 });
 
 export default sequelize;
