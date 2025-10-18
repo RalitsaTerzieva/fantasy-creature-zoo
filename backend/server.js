c84b4ed58198b1b82c3ab114ab104fe155106208
Ralica Popova <ralicaterzieva90@gmail.com>
2025-10-18 18:41:55 +0300

    Update server

--- backend/server.js
+++ backend/server.js
@@ -2,21 +2,29 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import sequelize from "./config/db.js";
+import "./models/index.js"; 
 import creatureRoutes from "./routes/creatureRoutes.js";
 import habitatRoutes from "./routes/habitatRoutes.js";
 
 dotenv.config();
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:5173",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type"]
+}));
 app.use(express.json());
 
-app.use("api/creatures", creatureRoutes);
-app.use("api/habitat", habitatRoutes);
+app.use("/api/creatures", creatureRoutes);
+app.use("/api/habitat", habitatRoutes);
 
-sequelize.sync().then(() => {
-  console.log("✅ Database synced!");
-  app.listen(5000, () => console.log("🚀 Server running on port 5000"));
-});
+sequelize
+  .sync({ alter: true })
+  .then(() => {
+    console.log("✅ Database synced!");
+    app.listen(4000, () => console.log("🚀 Server running on port 4000"));
+  })
+  .catch((err) => console.error("❌ Sync error:", err));
 
 
