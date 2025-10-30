8e67368a267d3fefb133c2976e32ec72d21cdccf
Ralica Popova <ralicaterzieva90@gmail.com>
2025-10-30 18:33:40 +0200

    Add new endpoint

--- backend/routes/creatureRoutes.js
+++ backend/routes/creatureRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { getCreatures, addCreature } from "../controllers/createController.js";
+import { getCreatures, addCreature, getCreatureById } from "../controllers/createController.js";
 
 const router = express.Router();
 
 router.get('/', getCreatures);
 router.post('/', addCreature);
+router.get('/profile-creature/:id', getCreatureById);
 
 export default router;
