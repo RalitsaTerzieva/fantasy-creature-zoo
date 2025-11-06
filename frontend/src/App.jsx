66f2b40ee943233b8f7acc072a0f7ac55def9da6
Ralica Popova <ralicaterzieva90@gmail.com>
2025-11-06 18:58:22 +0200

    Add Routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,19 @@
 import CreatureList from "./components/CreatureList";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
-    <div className="app-container">
-      <h1>Fantasy Creature Zoo 🐉</h1>
-      <CreatureList />
-    </div>
+    <Router>
+      <div className="app-container">
+        <h1>Fantasy Creature Zoo 🐉</h1>
+        <Routes>
+          <Route path="/" element={<CreatureList />} />
+
+          <Route path="/creature/:id" element={<CreatureProfile />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
