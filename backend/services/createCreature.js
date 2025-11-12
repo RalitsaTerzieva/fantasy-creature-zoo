18f31e29da02304a98b7f35dd2182f75a3adeee4
Ralica Popova <ralicaterzieva90@gmail.com>
2025-11-12 16:41:50 +0200

    Add service layer

---
+++ backend/services/createCreature.js
@@ -0,0 +1,22 @@
+import { Creature, Habitat } from "../models/index.js";
+
+export const getAllCreatures = async () => {
+  return await Creature.findAll({ include: Habitat });
+};
+
+
+export const createCreature = async (data) => {
+  // Here you could also validate, log, or transform the data later
+  const creature = await Creature.create(data);
+  return creature;
+};
+
+export const getCreatureById = async (id) => {
+  const creature = await Creature.findByPk(id, { include: Habitat });
+  if (!creature) {
+    const error = new Error("Creature not found");
+    error.status = 404; // So the controller or middleware can respond accordingly
+    throw error;
+  }
+  return creature;
+};
